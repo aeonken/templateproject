08ac1e781275b438f3b874e0b8ce4f1aa15545cf
sirisha illa <aeonken@sirishas-iMac.local>
2018-06-20 23:13:23 +0530

    intial version

---
+++ {{cookiecutter.repo_name}}/app/src/main/{{ cookiecutter.package_path }}/MainActivity.kt
@@ -0,0 +1,9 @@
+package {{ cookiecutter.package_name }}
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    
+}
